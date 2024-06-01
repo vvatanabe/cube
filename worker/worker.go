08f048a87dc24c1c4df3f76f19fc02b2d9e63f1a
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-01 17:42:04 +0900

    chore: Add CollectStats, RunTask, StartTask, and StopTask methods to Worker struct

--- worker/worker.go
+++ worker/worker.go
@@ -1,6 +1,7 @@
 package worker
 
 import (
+	"fmt"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 	"github.com/vvatanabe/cube/task"
@@ -11,4 +12,20 @@ type Worker struct {
 	Queue     queue.Queue
 	Db        map[uuid.UUID]*task.Task
 	TaskCount int
+}
+
+func (w *Worker) CollectStats() {
+	fmt.Println("I will collect stats")
+}
+
+func (w *Worker) RunTask() {
+	fmt.Println("I will start or stop a task")
+}
+
+func (w *Worker) StartTask() {
+	fmt.Println("I will start a task")
+}
+
+func (w *Worker) StopTask() {
+	fmt.Println("I will stop a task")
 }
