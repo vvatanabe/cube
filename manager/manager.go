c99148a204d468803e63cfa7ae9aa50a7b12a200
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-03 09:53:55 +0900

    feat: Add SelectWorker, UpdateTasks, and SendWork methods to Manager struct

--- manager/manager.go
+++ manager/manager.go
@@ -1,6 +1,7 @@
 package manager
 
 import (
+	"fmt"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 	"github.com/vvatanabe/cube/task"
@@ -13,4 +14,16 @@ type Manager struct {
 	Workers       []string
 	WorkerTaskMap map[string][]uuid.UUID
 	TaskWorkerMap map[uuid.UUID]string
+}
+
+func (m *Manager) SelectWorker() {
+	fmt.Println("I will select an appropriate worker")
+}
+
+func (m *Manager) UpdateTasks() {
+	fmt.Println("I will update tasks")
+}
+
+func (m *Manager) SendWork() {
+	fmt.Println("I will send work to workers")
 }
