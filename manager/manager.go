1234d97b97ca0ee1b44031ee80ab80dd847edd08
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-03 09:53:20 +0900

    feat: Add Manager struct to handle task management

--- manager/manager.go
+++ manager/manager.go
@@ -1,1 +1,16 @@
 package manager
+
+import (
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/vvatanabe/cube/task"
+)
+
+type Manager struct {
+	Pending       queue.Queue
+	TaskDb        map[string][]*task.Task
+	EventDb       map[string][]*task.TaskEvent
+	Workers       []string
+	WorkerTaskMap map[string][]uuid.UUID
+	TaskWorkerMap map[uuid.UUID]string
+}
