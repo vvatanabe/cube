2dda36544651183c02ddf5588c7e5b1c969d129a
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-04 09:36:04 +0900

    The Config struct that will hold the configuration for orchestration tasks

--- task/task.go
+++ task/task.go
@@ -36,3 +36,18 @@ type TaskEvent struct {
 	Timestamp time.Time
 	Task      Task
 }
+
+type Config struct {
+	Name          string
+	AttachStdin   bool
+	AttachStdout  bool
+	AttachStderr  bool
+	ExposedPorts  nat.PortSet
+	Cmd           []string
+	Image         string
+	Cpu           float64
+	Memory        int64
+	Disk          int64
+	Env           []string
+	RestartPolicy string
+}
