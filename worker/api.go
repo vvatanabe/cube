6dc294a94a2ad3324fa2c5e41089989227680f60
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-08 18:14:41 +0900

    The initRouter() method

--- worker/api.go
+++ worker/api.go
@@ -1,6 +1,8 @@
 package worker
 
-import "github.com/go-chi/chi/v5"
+import (
+	"github.com/go-chi/chi/v5"
+)
 
 type ErrResponse struct {
 	HTTPStatusCode int
@@ -12,4 +14,15 @@ type Api struct {
 	Port    int
 	Worker  *Worker
 	Router  *chi.Mux
+}
+
+func (a *Api) initRouter() {
+	a.Router = chi.NewRouter()
+	a.Router.Route("/tasks", func(r chi.Router) {
+		r.Post("/", a.StartTaskHandler)
+		r.Get("/", a.GetTasksHandler)
+		r.Route("/{taskID}", func(r chi.Router) {
+			r.Delete("/", a.StopTaskHandler)
+		})
+	})
 }
