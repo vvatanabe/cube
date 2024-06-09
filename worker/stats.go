cd59309d778b40807f4e0221a9172e35a28f83ae
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-09 12:56:29 +0900

    Using the CpuUsage() method to get CPU usage as a percentage

--- worker/stats.go
+++ worker/stats.go
@@ -36,3 +36,16 @@ func (s *Stats) DiskFree() uint64 {
 func (s *Stats) DiskUsed() uint64 {
 	return s.DiskStats.Used
 }
+
+func (s *Stats) CpuUsage() float64 {
+	idle := s.CpuStats.Idle + s.CpuStats.IOWait
+	nonIdle := s.CpuStats.User + s.CpuStats.Nice + s.CpuStats.System +
+		s.CpuStats.IRQ + s.CpuStats.SoftIRQ + s.CpuStats.Steal
+	total := idle + nonIdle
+
+	if total == 0 {
+		return 0.00
+	}
+
+	return (float64(total) - float64(idle)) / float64(total)
+}
