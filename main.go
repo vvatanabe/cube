b1fdda16d32899d4d4826a903724cdb3651a484e
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-15 06:47:36 +0900

    Starting up the worker API

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"github.com/golang-collections/collections/queue"
+	"github.com/google/uuid"
+	"github.com/vvatanabe/cube/task"
+	"github.com/vvatanabe/cube/worker"
 	"os"
 	"strconv"
 )
@@ -11,4 +16,15 @@ func main() {
 
 	mhost := os.Getenv("CUBE_MANAGER_HOST")
 	mport, _ := strconv.Atoi(os.Getenv("CUBE_MANAGER_PORT"))
+
+	fmt.Println("Starting Cube worker")
+
+	w := worker.Worker{
+		Queue: *queue.New(),
+		Db:    make(map[uuid.UUID]*task.Task),
+	}
+	wapi := worker.Api{Address: whost, Port: wport, Worker: &w}
+	go w.RunTasks()
+	go w.CollectStats()
+	go wapi.Start()
 }
