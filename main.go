2330c6cc8eb2ef6218a923246714b39e627cffb2
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-15 06:46:37 +0900

    Extracting the host and port for each API from environment variables

--- main.go
+++ main.go
@@ -1,58 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"github.com/golang-collections/collections/queue"
-	"github.com/google/uuid"
-	"github.com/vvatanabe/cube/manager"
-	"github.com/vvatanabe/cube/task"
-	"github.com/vvatanabe/cube/worker"
 	"os"
 	"strconv"
-	"time"
 )
 
 func main() {
-	host := os.Getenv("CUBE_HOST")
-	port, _ := strconv.Atoi(os.Getenv("CUBE_PORT"))
+	whost := os.Getenv("CUBE_WORKER_HOST")
+	wport, _ := strconv.Atoi(os.Getenv("CUBE_WORKER_PORT"))
 
-	fmt.Println("Starting Cube worker")
-
-	w := worker.Worker{
-		Queue: *queue.New(),
-		Db:    make(map[uuid.UUID]*task.Task),
-	}
-	api := worker.Api{Address: host, Port: port, Worker: &w}
-
-	go w.RunTasks()
-	go w.CollectStats()
-	go api.Start()
-
-	workers := []string{fmt.Sprintf("%s:%d", host, port)}
-	m := manager.New(workers)
-
-	for i := 0; i < 3; i++ {
-		t := task.Task{
-			ID:    uuid.New(),
-			Name:  fmt.Sprintf("test-container-%d", i),
-			State: task.Scheduled,
-			Image: "strm/helloworld-http",
-		}
-		te := task.TaskEvent{
-			ID:    uuid.New(),
-			State: task.Running,
-			Task:  t,
-		}
-		m.AddTask(te)
-		m.SendWork()
-	}
-
-	go m.UpdateTasks()
-
-	for {
-		for _, t := range m.TaskDb {
-			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
-			time.Sleep(15 * time.Second)
-		}
-	}
+	mhost := os.Getenv("CUBE_MANAGER_HOST")
+	mport, _ := strconv.Atoi(os.Getenv("CUBE_MANAGER_PORT"))
 }
