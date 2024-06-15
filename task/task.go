bc964e1d959558d2b0fe3b11b4a432caa715465d
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-15 18:13:29 +0900

    The Inspect method calling the Docker API

--- task/task.go
+++ task/task.go
@@ -163,6 +163,18 @@ func (d *Docker) Stop(id string) DockerResult {
 	return DockerResult{Action: "stop", Result: "success", Error: nil}
 }
 
+func (d *Docker) Inspect(containerID string) DockerInspectResponse {
+	dc, _ := client.NewClientWithOpts(client.FromEnv)
+	ctx := context.Background()
+	resp, err := dc.ContainerInspect(ctx, containerID)
+	if err != nil {
+		log.Printf("Error inspecting container: %s\n", err)
+		return DockerInspectResponse{Error: err}
+	}
+
+	return DockerInspectResponse{Container: &resp}
+}
+
 func NewDocker(c *Config) *Docker {
 	dc, _ := client.NewClientWithOpts(client.FromEnv)
 	return &Docker{
