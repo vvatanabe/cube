d79b5559ac5ef3582b7f32799b0001ca890076d8
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-15 18:36:50 +0900

    Adding the HealthCheck and RestartCount fields

--- task/task.go
+++ task/task.go
@@ -36,10 +36,13 @@ type Task struct {
 	Memory        int64
 	Disk          int64
 	ExposedPorts  nat.PortSet
+	HostPorts     nat.PortMap
 	PortBindings  map[string]string
 	RestartPolicy string
 	StartTime     time.Time
 	FinishTime    time.Time
+	HealthCheck   string
+	RestartCount  int
 }
 
 type TaskEvent struct {
