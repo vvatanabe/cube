9be32c142b09e211e29001d6926731928fec35a2
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-17 06:37:30 +0900

    The SelectCandidateNodes method for the round-robin scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -1,5 +1,10 @@
 package scheduler
 
+import (
+	"github.com/vvatanabe/cube/node"
+	"github.com/vvatanabe/cube/task"
+)
+
 type Scheduler interface {
 	SelectCandidateNodes()
 	Score()
@@ -9,4 +14,8 @@ type Scheduler interface {
 type RoundRobin struct {
 	Name       string
 	LastWorker int
+}
+
+func (r *RoundRobin) SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node {
+	return nodes
 }
