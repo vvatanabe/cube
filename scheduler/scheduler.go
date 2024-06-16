1c3ab8d3275364db2f9fb5f952ab28376e040874
vvatanabe <yuichi.watanabe.ja@gmail.com>
2024-06-17 06:40:36 +0900

    The Score method

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -19,3 +19,25 @@ type RoundRobin struct {
 func (r *RoundRobin) SelectCandidateNodes(t task.Task, nodes []*node.Node) []*node.Node {
 	return nodes
 }
+
+func (r *RoundRobin) Score(t task.Task, nodes []*node.Node) map[string]float64 {
+	nodeScores := make(map[string]float64)
+	var newWorker int
+	if r.LastWorker+1 < len(nodes) {
+		newWorker = r.LastWorker + 1
+		r.LastWorker++
+	} else {
+		newWorker = 0
+		r.LastWorker = 0
+	}
+
+	for idx, node := range nodes {
+		if idx == newWorker {
+			nodeScores[node.Name] = 0.1
+		} else {
+			nodeScores[node.Name] = 1.0
+		}
+	}
+
+	return nodeScores
+}
